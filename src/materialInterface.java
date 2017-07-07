d0216bf82473cda84f8e00c31acb37c296cd7a4e
costaskorai <costaskorai@gmail.com>
2017-07-07 22:09:11 +1000

    updates to composite storage for materials

--- src/materialInterface.java
+++ src/materialInterface.java
@@ -2,7 +2,27 @@
  * Created by Tesseract on 6/07/2017.
  */
 
+//the materials have these attributes MATERIAL,HP,TYPE,TIME,LOCATION
+//the attributes should be held in the base class and the composite no variables in the interface
+
+// so what methods do i need for these materials
+
 public interface materialInterface {
 
-    public void smFunc();
+    //getters
+    public void getMaterial();  //using these will limits modularity but any new system would need new storage
+    public void getHP();
+    public void getType();
+    public void getLocation();
+
+    //setters
+    public void setMaterial();
+    public void setHP();
+    public void setType();
+    public void setLocation();
+
+    //what else will they both need to do ?? children yes
+    //they should both hold a list of children( the base materials that make them)
+
+    public void addChild(); //??what is the input
 }
